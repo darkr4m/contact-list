ea726f43ebb097c3d376bd3fd75c38cab9b5ab4f
max <hmwright404@gmail.com>
2025-02-05 16:13:45 -0600

    add add_contact and remove_contact

--- contact_list.py
+++ contact_list.py
@@ -19,4 +19,36 @@ class ContactList:
 
 
     """
+    def __init__(self,name,contacts):
+        self._name = name
+        self._contacts = sorted(contacts, key=lambda x: x['name'])
+
+    # Getters and setters
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self,name):
+        if isinstance(name, str):
+            self._name = name
+        else:
+            print("Name must be a string.")
     
+    @property
+    def contacts(self):
+        return self._contacts
+    @contacts.setter
+    def contacts(self,contacts):
+        self._contacts = contacts
+
+    def add_contact(self, contact):
+        contacts = self.contacts
+        contacts.append(contact)
+        self.contacts = sorted(contacts, key=lambda x: x['name'])
+
+    def remove_contact(self,name):
+        contacts = [c for c in self.contacts if c['name'] != name]
+        print(contacts)
+
+
+list = ContactList('Me', [{'name':'Mac','number':'444-2231'}])

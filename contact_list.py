683c6dd52bda2a90a5b2ed37025c5132b9fbfa08
max <hmwright404@gmail.com>
2025-02-05 15:34:49 -0600

    clone and pseudocode

--- contact_list.py
+++ contact_list.py
@@ -1,2 +1,22 @@
 class ContactList:
-    pass
+    """
+    Attributes
+        list_name
+        contacts = []
+
+    contact = 
+        name - string
+        number - string
+        {
+            "name": "Alice",
+            "number":"555-5555"
+        }
+
+    methods
+        add_contact({})
+        remove_contact(name)
+        find_shared_contacts(ContactList)
+
+
+    """
+    
